e8b78ed3cd537c2d1d0ed6bd75e0c803f16113e6
vergi <christian.vergi@gmail.com>
2020-08-26 14:46:52 +0700

    Initial commit

---
+++ src/main/java/com/enigma/mybel/constants/Constant.java
@@ -0,0 +1,14 @@
+package com.enigma.mybel.constants;
+
+public class Constant {
+    public static final String FILENOTFOUND="File not found";
+    public static final String RESOURCENOTFOUND="Resource with id %s not found";
+    public static final String FOLDERPATH="/home/vergi/Documents/materipascal/FinalProjectBackend/mybel-backend-master/src/main/resources/%s.jpg";
+    //public static final String FOLDERPATH="/services/uploads/%s.png";
+    public static final String PATHVENDOR="/vendor/photo/%s";
+    public static final String PATHUNIT="/unit/photo/%s";
+    public static final String PATHDESIGN="/design/photo/%s";
+    public static final String PATHUSER="/user/photo/%s";
+    public static final String RESOURCEISEXIST="%s is exist";
+
+}
